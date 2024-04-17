51b46a6708f1fb1fa403e43e811a3eaced53fa70
Damir <ah710254@gmail.com>
2024-04-17 14:00:39 +0300

    allah4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,4 +2,4 @@
 import { readlineSync } from 'readline-sync';
 console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('Your answer: ');
-console.log(name)
+console.log(name);
